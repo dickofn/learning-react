671a0bbbe13c89c282afd3f67ba785fec3345989
Dicko Finus <dickofn@gmail.com>
2021-01-09 14:00:17 +0700

    Added tailwind, craco (for tailwind), fontawesome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <HelloWorld name="Dicko"></HelloWorld>
+      <Footer />
     </div>
   );
 }
