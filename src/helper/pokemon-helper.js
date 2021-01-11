1bec8bd1fba3b32d1bf66158281e78fccebbec7a
Dicko Finus <dickofn@gmail.com>
2021-01-11 17:55:46 +0700

    Single pokemon page dynamic color

---
+++ src/helper/pokemon-helper.js
@@ -0,0 +1,54 @@
+const colours = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
+
+export function getColorByType(type) {
+  if (type) {
+    return colours[type];
+  } else {
+    return "rgba(255, 255, 255, 0)";
+  }
+}
+
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+}
+
+export function getContrastTextColorByType(type) {
+  const { r, g, b } = hexToRgb(colours[type]);
+  if (r * 0.299 + g * 0.587 + b * 0.114 > 186) {
+    return "#222";
+  } else {
+    return "#eee";
+  }
+}
--- src/views/Pokemon.js
+++ src/views/Pokemon.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGem } from "@fortawesome/free-solid-svg-icons";
+import { faGem as farGem } from "@fortawesome/free-regular-svg-icons";
+
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { getContrastTextColorByType } from "../helper/pokemon-helper";
+
 import Loader from "../components/Loader";
 
-function Product() {
+function Pokemon() {
   const { id } = useParams();
 
   const GET_POKEMON = gql`
@@ -11,21 +21,33 @@ function Product() {
       pokemon(name: $name) {
         id
         name
+        height
+        weight
+        types {
+          type {
+            name
+          }
+        }
+        stats {
+          stat {
+            name
+          }
+          base_stat
+        }
         sprites {
           front_default
+          front_shiny
+        }
+        abilities {
+          ability {
+            name
+          }
         }
         moves {
           move {
             name
           }
         }
-        types {
-          type {
-            name
-          }
-        }
-        height
-        weight
       }
     }
   `;
@@ -48,19 +70,56 @@ function Product() {
     content = <div className="text-red-500"> {id} is not registered yet</div>;
   }
 
+  const [isShiny, setIsShiny] = useState(false);
+
   if (data) {
+    const pokemonType = data.pokemon.types[0].type.name;
+
+    const contrastTextStyle = css`
+      color: ${getContrastTextColorByType(pokemonType)};
+    `;
+
     content = (
-      <div>
-        <h1 className="text-2xl font-bold mb-3 uppercase text-center">
+      <div className="px-4">
+        <h1
+          className={
+            "-mx-4 py-3 text-2xl font-bold mb-3 uppercase text-center text-white bg-" +
+            pokemonType
+          }
+          css={contrastTextStyle}
+        >
           {data.pokemon.name}
         </h1>
 
         <div>
           <img
-            src={data.pokemon.sprites.front_default}
+            src={
+              isShiny
+                ? data.pokemon.sprites.front_shiny
+                : data.pokemon.sprites.front_default
+            }
             alt={data.pokemon.name}
-            className="mx-auto"
+            className="mx-auto h-40 w-40"
           />
+        </div>
+
+        <div className="text-center">
+          <button
+            onClick={() => setIsShiny(!isShiny)}
+            className={
+              "rounded-full py-3 px-5 flex justify-center items-center ml-auto transition-all duration-200 focus:outline-none hover:bg-opacity-90 bg-" +
+              pokemonType
+            }
+            css={contrastTextStyle}
+          >
+            <div className="flex justify-center items-center">
+              <FontAwesomeIcon
+                icon={isShiny ? faGem : farGem}
+                className="text-2xl mr-2"
+              />
+              <div className="font-bold">Shiny</div>
+            </div>
+          </button>
         </div>
 
         <div className="font-bold text-xl mb-3">{data.pokemon.weight} kg</div>
@@ -73,4 +132,4 @@ function Product() {
   return <div>{content}</div>;
 }
 
-export default Product;
+export default Pokemon;
