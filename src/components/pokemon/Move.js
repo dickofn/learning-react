1f5752a5d173427d2f84c10edca7ecb714a53c11
Dicko Finus <dickofn@gmail.com>
2021-01-11 23:59:46 +0700

    Updated move

--- src/components/pokemon/Move.js
+++ src/components/pokemon/Move.js
@@ -1,9 +1,46 @@
-function Move() {
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { getContrastTextColorByType } from "../../helper/pokemon-helper";
+
+function Move(props) {
+  const data = props.data;
+
+  console.log(data);
+
+  const abilities = data.abilities.map((i) => {
+    const abilityName = (
+      i.ability.name.charAt(0).toUpperCase() + i.ability.name.slice(1)
+    )
+      .split("-")
+      .join(" ");
+    return <div key={i.ability.name}>{abilityName}</div>;
+  });
+
+  const moves = data.moves.map((i) => {
+    const moveName = (
+      i.move.name.charAt(0).toUpperCase() + i.move.name.slice(1)
+    )
+      .split("-")
+      .join(" ");
+    return <div key={i.move.name}>{moveName}</div>;
+  });
+
   return (
-    <div>
-      
+    <div className="p-4">
+      <table className="w-full">
+        <tbody>
+          <tr>
+            <td className="font-medium align-baseline">Abilities</td>
+            <td className="pb-3">{abilities}</td>
+          </tr>
+          <tr>
+            <td className="font-medium align-baseline">Moves</td>
+            <td>{moves}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Move
+export default Move;
