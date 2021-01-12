4218661ffc0b197d7dd9adedd9a86a84d48d96c1
Dicko Finus <dickofn@gmail.com>
2021-01-12 18:49:40 +0700

    Removed navigation from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 import { Link } from "react-router-dom";
-import Navigation from "./Navigation";
 
 function Header() {
   return (
     <header className="border-b p-3 flex justify-between items-center">
       <Link className="font-bold uppercase font-mono" to="/">Maybe Pokedex</Link>
-      <Navigation />
     </header>
   );
 }
